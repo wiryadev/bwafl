5037a08aa36752c373255a46631d82e83614f179
wiryadev <abrarwiryawan@gmail.com>
2021-12-06 12:43:54 +0700

    add product API endpoint

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $tags = $request->input('tags');
+        $categories = $request->input('categories');
+
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+        if ($id) {
+            $product = Product::with(['categories', 'galleries'])->find($id);
+
+            if ($product) {
+                return ResponseFormatter::success(
+                    $product,
+                    'Data product berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ditemukan',
+                    404
+                );
+            }
+            
+        }
+
+        $products = Product::with(['categories', 'galleries']);
+        
+        if ($name) {
+            $products->where('name', 'like', '%' . $name . '%');
+        }
+
+        if ($description) {
+            $products->where('description', 'like', '%' . $description . '%');
+        }
+
+        if ($tags) {
+            $products->where('tags', 'like', '%' . $tags . '%');
+        }
+
+        if ($price_from) {
+            $products->where('price', '>=', $price_from);
+        }
+
+        if ($price_to) {
+            $products->where('price', '<=', $price_to);
+        }
+
+        if ($categories) {
+            $products->where('categories', $categories);
+        }
+
+        return ResponseFormatter::success(
+            $products->paginate($limit),
+            'Data product berhasil diambil'
+        );
+    }
+}
