ab051056941a3dfe7e684bd6881efaf28beda5a8
wiryadev <abrarwiryawan@gmail.com>
2021-12-06 14:40:56 +0700

    add register API endpoint

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Rules\Password;
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ["required", "string", "max:255"],
+                'username' => ["required", "string", "max:255", "unique:users"],
+                'phone' => ["nullable", "string", "max:255"],
+                'email' => ["required", "string", "email", "max:255", "unique:users"],
+                'password' => ["required", "string", new Password],
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'phone' => $request->phone,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success(
+                [
+                    'access_token' => $tokenResult,
+                    'token_type' => "Bearer",
+                    'user' => $user
+                ],
+                "Registrasi Berhasil"
+            );
+        } catch (Exception $e) {
+            return ResponseFormatter::error(
+                [
+                    'message' => "Something went wrong",
+                    'error' => $e
+                ],
+                "Registrasi Gagal",
+                500
+            );
+        }
+    }
+}
