5e5bc84094695155defa9201bf27b140b4045ecf
wiryadev <abrarwiryawan@gmail.com>
2021-12-27 17:41:16 +0700

    add product category dashboard admin

---
+++ resources/views/pages/dashboard/category/index.blade.php
@@ -0,0 +1,59 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Category') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="script">
+        <script>
+            // Ajax datatables
+            var dataTable = $('#dataTable').DataTable({
+                ajax: {
+                    url: "{!! url()->current() !!}",
+                },
+                columns: [{
+                        data: 'id',
+                        name: 'id',
+                        width: '5%'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name',
+                        orderable: 'false',
+                        searchable: 'false',
+                        width: '25%',
+                    }
+                ]
+            })
+        </script>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mb-10">
+                <a href="{{ route('dashboard.category.create') }}" class="px-4 py-2 font-bold text-white bg-green-500 rounded shadow-lg hover:bg-green-700">
+                    + Create Category
+                </a>
+            </div>
+            <div class="overflow-hidden shadow sm:rounded-md">
+                <div class="px-4 py-5 bg-white sm:p-6">
+                    <table id="dataTable" class="w-full table-auto">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
